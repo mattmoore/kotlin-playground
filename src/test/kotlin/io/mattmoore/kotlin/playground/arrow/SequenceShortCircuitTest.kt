b9a9fa94f8f70d5e4fa8920c911202e293048efa
Matt Moore <matt@mattmoore.io>
2020-04-28 20:55:56 -0500

    Add error scenario for sequence example.

--- src/test/kotlin/io/mattmoore/kotlin/playground/arrow/SequenceShortCircuitTest.kt
+++ src/test/kotlin/io/mattmoore/kotlin/playground/arrow/SequenceShortCircuitTest.kt
@@ -10,21 +10,21 @@ import arrow.core.extensions.list.traverse.sequence
 import java.io.File
 
 class SequenceShortCircuitTest : StringSpec({
+    // We can define a function `readFile` that returns an Either<E, A>
+    fun readFile(file: String): Either<AppError.FileFailed, String> =
+            try {
+                Right(File(file).readText())
+            } catch (e: Exception) {
+                Left(AppError.FileFailed(e.localizedMessage))
+            }
+
+    // We're defining a function to load all the files, but short-circuit when one of them fails.
+    // Sequence applies to a list of effects to be run.
+    // This is different from traverse, which applies an effectful function to a list of pure values.
+    fun loadShortCircuit(files: List<Either<AppError.FileFailed, String>>) =
+            files.sequence(Either.applicative())
+
     "sequence is useful when you have a List<Either<E, A>> instead of a List<A>. in other words, a list of effects" {
-        // We can define a function `readFile` that returns an Either<E, A>
-        fun readFile(file: String): Either<Exception, String> =
-                try {
-                    Right(File(file).readText())
-                } catch (e: Exception) {
-                    Left(e)
-                }
-
-        // We're defining a function to load all the files, but short-circuit when one of them fails.
-        // Sequence applies to a list of effects to be run.
-        // This is different from traverse, which applies an effectful function to a list of pure values.
-        fun loadShortCircuit(files: List<Either<Exception, String>>) =
-                files.sequence(Either.applicative())
-
         val expected = Right(listOf(
                 "I'm the first part.\n",
                 "I'm the second part.\n",
@@ -38,6 +38,25 @@ class SequenceShortCircuitTest : StringSpec({
                 readFile("data/part2.txt"),
                 readFile("data/part3.txt"),
                 readFile("data/part4.txt")
+        )
+
+        val result = loadShortCircuit(effectList)
+
+        result shouldBe expected
+    }
+
+    "let's try the same thing, but see what happens when an error occurs" {
+        val effectList = listOf(
+                readFile("data/part1.txt"),
+                readFile("data/monkeywrench.txt"),
+                readFile("data/part3.txt"),
+                readFile("data/part4.txt")
+        )
+
+        val expected = Left(
+                AppError.FileFailed(
+                        "data/monkeywrench.txt (No such file or directory)"
+                )
         )
 
         val result = loadShortCircuit(effectList)
