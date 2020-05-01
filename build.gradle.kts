101cf26210b532930f27f5cf628b5923426dd74e
Matt Moore <matt@mattmoore.io>
2020-05-01 07:37:50 -0500

    Minor tweaks to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,13 @@ repositories {
     // Use jcenter for resolving dependencies.
     // You can declare any Maven/Ivy/file repository here.
     jcenter()
+    maven(url = "https://oss.jfrog.org/artifactory/oss-snapshot-local/")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 tasks.withType<Test> {
@@ -29,7 +32,7 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
     // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
     // Kotest
     testImplementation("io.kotest:kotest-runner-junit5-jvm:4.0.5")
