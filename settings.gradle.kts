6b55300d22bfe5189e4ad4ace66a61456a6087a3
Matt Moore <matt@mattmoore.io>
2020-07-10 13:17:01 -0500

    Add IR Transform project.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "kotlin-playground"
-include("plugins:annotation-processor:compiler-plugin", "plugins:annotation-processor:use-plugin")
+include(
+  "plugins:annotation-processor:compiler-plugin",
+  "plugins:annotation-processor:use-plugin"
+)
+include(
+  "plugins:ir-transform:compiler-plugin",
+  "plugins:ir-transform:use-plugin"
+)
